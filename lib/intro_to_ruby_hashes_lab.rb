962097391474566711e67f74e954d16ee9d9d895
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-01 18:02:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,20 +3,20 @@ def new_hash
 end
 
 def my_hash
-  hash = {
+ {
     :key => "random"
   }
 end
 
 def pioneer
- hash = {
+ {
    :name => "Grace Hopper"
  }
 end
 
 
 def id_hash_generator(number)
-  hash = {
+{
     :id => number
   }
 end
